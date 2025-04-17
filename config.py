0725b55ca02c3274b18094e13f2c0bc65c7340d9
kkenz <k.28.06.2002@gmail.com>
2025-04-17 18:53:39 +0500

    refactor(config): move API_TOKEN to environment variable
    The API_TOKEN is now fetched from the environment variable using `os.getenv` for better security and configurability. This change aligns with best practices for managing sensitive information and allows for easier configuration without modifying the codebase directly. Additionally, the `.env` file has been added to store the API_TOKEN securely.

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-API_TOKEN = '7089277164:AAFASyyXhuBvZHQLj8S6QIAMgSvEE62HNf4'
+import os
+API_TOKEN = os.getenv("API_TOKEN")
 
 # Используем IP-адрес напрямую, чтобы обойти проблемы с DNS
-API_SERVER = 'https://149.154.167.220'  # IP address of api.telegram.org 
+API_SERVER = 'https://149.154.167.220'  # IP address of api.telegram.org
