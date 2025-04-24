ceae49cc3de55ec193497fc805a13fbf5819e17a
kkenz <k.28.06.2002@gmail.com>
2025-04-24 16:02:18 +0500

    chore: update API_TOKEN in config.py
    The API_TOKEN has been updated to ensure secure and uninterrupted access to the Telegram API.

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-API_TOKEN ="7089277164:AAFASyyXhuBvZHQLj8S6QIAMgSvEE62HNf4"
+API_TOKEN ="7089277164:AAGiT0keKQwQ0YddKbzXflrrjap0eAvBg0g"
 
 # Используем стандартный домен Telegram API
 API_SERVER = 'https://api.telegram.org'
